5bccf4cfcaedffdeaa99852bc0b26ed462bd7492
kendadada <153286742+kendadada@users.noreply.github.com>
2023-12-11 13:50:59 +0900

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 一覧
+Route::get('todo', [TodoController::class, 'index']);
+
+//作成
+Route::post('/create', [TodoController::class, 'create']);
+Route::get('/todo_create-page', [TodoController::class, 'todo_create']);
+
+// 更新
+Route::get('/todo_update-page/{id}', [TodoController::class, 'todo_update']);
+Route::post('/update/{id}', [TodoController::class, 'update'])->name('todo.update');
+
+// 削除
+Route::get('/todo_delete-page/{id}', [TodoController::class, 'todo_delete']);
+Route::post('/delete/{id}', [TodoController::class, 'delete'])->name('todo.delete');
