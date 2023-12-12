8c331f484f0911bd1098e1cce3e40ff8f44e6941
kendadada <153286742+kendadada@users.noreply.github.com>
2023-12-12 17:02:01 +0900

    Update todo_view-page.blade.php

--- resources/views/todo_view-page.blade.php
+++ resources/views/todo_view-page.blade.php
@@ -5,42 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/todo_view.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
     <title>Todo list</title>
     @LivewireStyles
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/index.js', 'resources/css/index.css'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">ToDo</a>
+            </div>
+        </nav>
+    </header>
     <div class="container">
-        <h1>ToDo List</h1>                                                                                                 
-        
+        <h1>ToDo List</h1>
         <h2>タスクの一覧</h2>
         <a href="/todo_create-page">タスクを追加</a>
+        
         @foreach($todos as $todo)
-        <div style="mini-container">
-            
+        <div class="KanbanCard">
             <h4>{{$todo->task_name}}</h4>
-            
+            <p>{{$todo->task_description}}</p>
+            <p>{{$todo->assing_person_name}}</p>
+            <p>{{$todo->estimate_hour}}</p>
+            <a href="/todo_update-page/{{$todo->id}}">編集</a>
+            <a href="/todo_delete-page/{{$todo->id}}">削除</a>
         </div>
         @endforeach
-        <table border="1">
-            <tr>
-                <th>タスクの名前</th>
-                <th>タスクの説明</th>
-                <th>担当者の名前</th>
-                <th>見積時間(h)</th>
-                <th colspan="2">操作</th>
-            </tr>
-            @foreach($todos as $todo)
-            <tr>
-                <td>{{$todo->task_name}}</td>
-                <td>{{$todo->task_description}}</td>
-                <td>{{$todo->assing_person_name}}</td>
-                <td>{{$todo->estimate_hour}}</td>
-                <td><a href="/todo_update-page/{{$todo->id}}">編集</a></td>
-                <td><a href="/todo_delete-page/{{$todo->id}}">削除</a></td>
-            </tr>
-            @endforeach
-        </table>
     </div>
     <a href="/home">ホームへ</a>
 </body>
